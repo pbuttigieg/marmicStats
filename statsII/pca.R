c8e9901856375f7f73067fa4610a8269806932a1
Pier Luigi Buttigieg <p.buttigieg@gmail.com>
2016-02-22 10:43:52 +0100

    updated some formatting

--- statsII/pca.R
+++ statsII/pca.R
@@ -12,10 +12,21 @@ data(varespec)
 summary(varespec)
 head(varespec)
 dim(varespec)
-corrgram(varespec,lower.panel = panel.shade, upper.panel = NULL, text.panel=panel.txt)
+
+# Let's build a correlelogram 
+corrgram(
+  varespec,
+  lower.panel = panel.shade,
+  upper.panel = NULL,
+  text.panel = panel.txt
+  )
 
 ### perform PCA 
-myPca <- rda(varespec, scale=TRUE)
+myPca <- rda(
+  varespec, 
+  scale=TRUE
+  )
+
 plot(myPca)
 biplot(myPca, choices = 1:2, scaling=-1)
 biplot(myPca, choices = 1-3, scaling=-1)
@@ -23,6 +34,7 @@ biplot(myPca, choices = 1-3, scaling=-1)
 ### total variance
 sum(myPca$CA$eig)
 barplot(myPca$CA$eig,cex.names = 0.8,las=2)
+screeplot(myPca)
 
 ### variance captured in the first 3 PCs
 (sum(myPca$CA$eig[1:3])/sum(myPca$CA$eig))*100
