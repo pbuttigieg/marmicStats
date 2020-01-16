d2d410e9d8d33865d8b4f2bc06cc9f1ab34d1488
CoraHoerstmann <44617344+CoraHoerstmann@users.noreply.github.com>
2020-01-16 11:47:41 +0100

    Update clustering.R
    update comparing the clustering methods

--- statsII/clustering.R
+++ statsII/clustering.R
@@ -39,7 +39,19 @@ plot(dune.cc)
 
 rbind(cutree(dune.wc,k=5),cutree(dune.cc,k=5))
 
-### hierarchical clustering based on PCoA first 2 axes
+#to compare the two clustering methods
+#nmds ordination overlapped with average method
+MyNmds.eu<-metaMDS(comm=dune, distance="euclidean", k=2, trymax = 100,trace = TRUE)
+dune.cc <- hclust(dune.eu,method = "complete")
+plot(MyNmds.eu, type="t", display ="sites")
+ordicluster(MyNmds.eu, dune.cc, col=cutree(dune.cc,5), prune = 3)
+#nmds ordination overlapped with ward method
+MyNmds.eu<-metaMDS(comm=dune, distance="euclidean", k=2, trymax = 100,trace = TRUE)
+dune.wc <- hclust(dune.eu,method = "ward")
+plot(MyNmds.eu, type="t", display ="sites")
+ordicluster(MyNmds.eu, dune.wc, col=cutree(dune.wc,5), prune = 3)
+
+### hierarchical clustering based on PCA first 2 axes
 myPca <- rda(dune,scale=TRUE)
 dune.eu.dist2 <- dist(myPca$CA$u[,1:2],method = "euclidean")
 dune.wc2 <- hclust(dune.eu.dist2,method = "ward.D2")
